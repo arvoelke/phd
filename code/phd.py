5425ae023ac14bf6732ce45ad7006b77fbc22303
Aaron Voelker <arvoelke@gmail.com>
2018-10-28 15:05:54 -0400

    Slotted in nengo forum post on peak detector challenge

---
+++ code/phd.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import nengo
+import nengolib
+from nengolib.signal import s, z
+
+import os
+
+figdir = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    '../figures')
+
+def savefig(name):
+    plt.savefig(os.path.join(figdir, name), dpi=600, bbox_inches='tight')
