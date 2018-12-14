ada54b8dc5ab05240817f3042c18e774e7495244
Aaron Voelker <arvoelke@gmail.com>
2018-12-14 16:15:15 -0500

    Added 200-trial Loihi integrator + both HW figures

---
+++ code/loihi-integrator.py
@@ -0,0 +1,151 @@
+# Adapted from _braindrop_integrator.ipynb
+# Must be on nengo_loihi branch=integrator-accuracy (PR #124)
+
+from phd import *
+from vens import VirtualEnsemble
+from dn import set_style
+set_style()
+
+import nengo_loihi
+nengo_loihi.set_defaults()
+
+import nengolib
+from nengolib.signal import s, nrmse
+from nengolib.synapses import ss2sim
+
+import warnings
+warnings.filterwarnings("ignore")  # otherwise 100+ MB of loihi warnings will crash your computer!
+
+
+def init_generators(sim, name='host_pre', rng=np.random):
+    """Jitters all of the spike-generators to reduce spike-synchrony."""
+    sim_host = sim.sims[name]
+    i = 0
+    for a in sim_host.model.sig:
+        if isinstance(a, nengo.Ensemble) and isinstance(a.neuron_type, nengo_loihi.neurons.NIF):
+            sim_host.signals[sim_host.model.sig[a.neurons]['voltage']] = rng.rand(a.n_neurons)
+            i += 1
+    return i
+
+
+def do_trial(n_neurons=300,
+             partition=None,
+             freq=1,
+             sim_t=10,
+             tau=0.2,
+             tau_probe=0.2,
+             dt=0.001,
+             input_seed=0,
+             ens_kwargs = {
+                 'neuron_type': nengo_loihi.neurons.LoihiSpikingRectifiedLinear(),
+             }):
+
+    # Discretized Principle 3 mapping of the integrator
+    A, B, C, D = ss2sim(~s, nengo.Lowpass(tau), dt=dt).ss
+    assert np.allclose(C, 1)
+    assert np.allclose(D, 0)
+
+    def loihi_factory(model, dt, tau=tau):
+        loihi_model = nengo_loihi.builder.Model(dt=dt)
+        loihi_model.inter_tau = tau  # used by spike generator
+        return nengo_loihi.Simulator(
+            model, model=loihi_model, precompute=True, dt=dt) 
+
+    # The integral of test_u is test_x, where test_x is [-1, 1] and
+    # starts at zero. 
+    process = nengo.processes.WhiteSignal(
+        period=sim_t, high=freq, y0=0, seed=input_seed)
+    test_x = process.run(sim_t, dt=dt)
+    test_x /= np.max(np.abs(test_x))
+    test_u = (test_x - np.roll(test_x, 1)) / dt
+
+    # Roll the signals so that it starts where both u and x are close to 0
+    w_x = 2  # used for Braindrop experiment
+    cost = test_u**2 + w_x*(test_x)**2
+    test_u = np.roll(test_u, -np.argmin(cost))
+    # don't need to roll test_x because we don't use it anywhere else
+    
+    # But due to:
+    #   https://github.com/nengo/nengo-loihi/issues/115
+    # we need to split the input B*u across multiple spike generators
+    # each normalized to [1, 1], and furthermore move the synapse
+    # to inter_tau. Both of these changes have additional benefits
+    # for accuracy as well, related to altering the noise floor
+    # introduced by spike generation, and scaling with dynamic range
+    # (number of generators created).
+    split = np.ceil(np.max(np.abs(B*test_u))).astype(int)
+
+    with nengo.Network() as model:
+        u = nengo.Node(output=nengo.processes.PresentInput(test_u, dt))
+
+        if partition is None:
+            x = nengo.Ensemble(n_neurons, 1, **ens_kwargs)
+
+            make_input_connection = nengo.Connection
+
+            nengo.Connection(x, x, transform=A, synapse=tau,
+                             solver=nengo.solvers.LstsqL2(weights=True))
+
+            p_x = nengo.Probe(x, synapse=tau_probe)
+
+        else:
+            if n_neurons % partition != 0:
+                raise ValueError("n_neurons (%s) must be divisible by partition (%s)" % (
+                    n_neurons, partition))
+
+            x = VirtualEnsemble(n_ensembles=n_neurons // partition,
+                                n_neurons_per_ensemble=partition,
+                                dimensions=1,
+                                **ens_kwargs)
+
+            def make_input_connection(pre, post, **kwargs):
+                return post.add_input(u, **kwargs)
+
+            x.add_input(x.add_output(dt=dt)[0], transform=A, synapse=tau)
+
+            # Copy the output so that the above is collapsed as a passthrough
+            p_x = nengo.Probe(x.add_output(dt=dt)[0], synapse=tau_probe)
+
+        for _ in range(split):
+            # Use synapse=None here because the spike generator
+            # will have a synapse of tau
+            make_input_connection(u, x, transform=B / split, synapse=None)
+
+        p_u = nengo.Probe(u, synapse=None)
+        p_ideal = nengo.Probe(u, synapse=nengolib.Lowpass(tau_probe) / s)
+
+    with loihi_factory(model, dt) as sim:
+        init_generators(sim)
+        sim.run(sim_t)
+        
+    return {
+        't': sim.trange(),
+        'u': sim.data[p_u],
+        'actual': sim.data[p_x],
+        'ideal': sim.data[p_ideal],
+    }
+
+
+if __name__ == '__main__':
+    data = []
+    for i in range(200):
+        print("Trial #", i)
+        data.append(do_trial(n_neurons=256, partition=None,
+                             freq=1, sim_t=10, input_seed=1,
+                             tau=0.2, tau_probe=0.2))
+
+    flattened = []
+    for r in data:
+        flattened.append(r['actual'])
+        # all r['t'] and r['ideal'] are identical
+
+    plt.figure(figsize=(3.5, 2))
+
+    blue, _, green = sns.color_palette('bright', 3)
+    plt.plot(r['t'], r['ideal'], c=green, linestyle='--', lw=1, label="Ideal")
+    sns.tsplot(flattened, r['t'], ci=95, color=blue, condition="Output")
+
+    plt.xlabel("Time (s)")
+    plt.legend()
+
+    savefig("loihi-integrator.pdf")
---
+++ code/vens.py
@@ -0,0 +1,134 @@
+"""https://forum.nengo.ai/t/how-many-neurons-can-be-fully-connected/706"""
+
+import numpy as np
+
+import nengo
+from nengo.params import IntParam
+from nengo.utils.builder import default_n_eval_points
+
+from nengo_loihi.builder import get_gain_bias, get_samples
+from nengo_loihi.neurons import loihi_rates
+
+
+class VirtualEnsemble(nengo.Network):
+    """Virtualize a single ensemble using multiple sub-ensembles.
+    
+    The naming comes from an analogy to "virtual memory" in PCs.
+    Since Loihi maps each ensemble to one core, large ensembles with
+    dense connection matrices can easily consume all of the memory.
+    A solution is to partition the ensemble across multiple cores,
+    and then jointly optimize for decoders across all sub-ensembles.
+    This network achieves this by configuring the tuning curves
+    in advance and stacking the optimization problems together to
+    connect up each output pre-build time. This provides an
+    Ensemble-like interface, that can be connected into and decoded
+    from, but is implemented using multiple sub-ensembles underneath.
+
+    TODO:
+     - document and test
+     - add_output assumes function is a callable
+     - label the ensembles, node, connections
+    """
+
+    n_ensembles = IntParam('n_ensembles', low=1)
+    
+    def __init__(self, n_ensembles, n_neurons_per_ensemble,
+                 intercept_limit=0.95, rng=np.random,
+                 label=None, seed=None, add_to_container=None,
+                 **ens_kwargs):
+
+        super(VirtualEnsemble, self).__init__(
+            label=label, seed=seed, add_to_container=add_to_container)
+        
+        for illegal in ('eval_points', 'n_eval_points'):
+            if illegal in ens_kwargs:
+                raise ValueError("Ensemble parameter '%s' is unsupported" % illegal)
+
+        self._ensembles = []
+        self.n_ensembles = n_ensembles
+        self.n_neurons_per_ensemble = n_neurons_per_ensemble
+
+        with self:
+            for _ in range(n_ensembles):
+                ens = nengo.Ensemble(n_neurons=n_neurons_per_ensemble, **ens_kwargs)
+
+                gain, bias, max_rates, intercepts = get_gain_bias(
+                    ens, rng=rng, intercept_limit=intercept_limit)
+
+                ens.gain = gain
+                ens.bias = bias
+                ens.max_rates = max_rates
+                ens.intercepts = intercepts
+
+                ens.encoders = get_samples(
+                    ens.encoders, ens.n_neurons, ens.dimensions, rng=rng)
+
+                self._ensembles.append(ens)
+                
+        # last ensemble is representative of all others in terms of dimensions
+        self.dimensions = ens.dimensions
+                
+    def add_input(self, pre, weights=True, **conn_kwargs):
+        if weights:
+            transform = np.asarray(conn_kwargs.get('transform', 1))
+        with self:
+            for post in self._ensembles:
+                if weights:
+                    conn_kwargs['transform'] = post.encoders.dot(transform)
+                    post = post.neurons
+                nengo.Connection(pre, post, **conn_kwargs)
+
+    def add_neuron_output(self):
+        with self:
+            output = nengo.Node(size_in=self.n_neurons_per_ensemble * self.n_ensembles)
+            for i, ens in enumerate(self._ensembles):
+                nengo.Connection(ens.neurons, output[i*ens.n_neurons:(i+1)*ens.n_neurons],
+                                 synapse=None)
+        return output
+                
+    def add_output(self,
+                   function=lambda x: x, 
+                   eval_points=nengo.dists.UniformHypersphere(surface=False),
+                   solver=nengo.solvers.LstsqL2(),
+                   dt=0.001,
+                   rng=np.random):
+
+        if not isinstance(eval_points, nengo.dists.Distribution):
+            raise TypeError("eval_points (%r) must be a "
+                            "nengo.dists.Distribution" % eval_points)
+        
+        n = self.n_ensembles * self.n_neurons_per_ensemble
+        n_points = default_n_eval_points(n, self.dimensions)
+        eval_points = eval_points.sample(n_points, self.dimensions, rng=rng)
+        
+        A = np.empty((n_points, n))
+        Y = np.asarray([np.atleast_1d(function(ep)) for ep in eval_points])
+        size_out = Y.shape[1]
+
+        for i, ens in enumerate(self._ensembles):
+            x = np.dot(eval_points, ens.encoders.T / ens.radius)
+            activities = loihi_rates(ens.neuron_type, x, ens.gain, ens.bias, dt)
+            A[:, i*ens.n_neurons:(i+1)*ens.n_neurons] = activities
+
+        D, info = solver(A, Y, rng=rng)  # AD ~ Y
+        assert D.shape == (n, size_out)
+
+        with self:
+            output = nengo.Node(size_in=size_out)
+            for i, ens in enumerate(self._ensembles):
+                # NoSolver work-around for Neurons -> Ensemble
+                # https://github.com/nengo/nengo-loihi/issues/152
+                # nengo.Connection(
+                #     ens, output, synapse=None,
+                #     solver=nengo.solvers.NoSolver(
+                #         D[i*ens.n_neurons:(i+1)*ens.n_neurons, :],
+                #         weights=False))
+                # TODO: investigate weird behaviour having something to do
+                #   with the function not being respected when the
+                #   add_output weights are embedded in NoSolver to form
+                #   a recurrent passthrough
+                nengo.Connection(
+                    ens.neurons, output, synapse=None,
+                    transform=D[i*ens.n_neurons:(i+1)*ens.n_neurons, :].T)
+
+        return output, info
